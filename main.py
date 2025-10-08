5aeebb56a412b525e8445517e89bbe5959eb7594
fernaerell <fernaerell2020@gmail.com>
2025-10-08 07:23:01 +0700

    chore(main): add var for set value of number_of_sections, symbol_table_offset, number_of_symbols

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-import binary, endian, coff
+import binary, endian, coff, datetime, math
 
 file_name = "main.obj"
 
@@ -9,16 +9,19 @@ coff_file_header = binary.size(20)
 binary.set(coff_file_header, 0, coff.IMAGE_FILE_MACHINE_AMD64, endian.LITTLE)
 
 # NumberOfSections (Offset 2, Size 2 byte)
-binary.set(coff_file_header, 2, binary.size(2), endian.LITTLE)
+number_of_sections = 0
+binary.set(coff_file_header, 2, number_of_sections.to_bytes(2, "big"), endian.LITTLE)
 
 # TimeDateStamp (Offset 4, Size 4 byte)
 binary.set(coff_file_header, 4, binary.size(4), endian.LITTLE)
 
 # PointerToSymbolTable (Offset 8, Ukuran 4 byte)
-binary.set(coff_file_header, 8, binary.size(4), endian.LITTLE)
+symbol_table_offset = 0
+binary.set(coff_file_header, 8, symbol_table_offset.to_bytes(4, "big"), endian.LITTLE)
 
 # NumberOfSymbols (Offset 12, Ukuran 4 byte)
-binary.set(coff_file_header, 12, binary.size(4), endian.LITTLE)
+number_of_symbols = 0
+binary.set(coff_file_header, 12, number_of_symbols.to_bytes(4, "big"), endian.LITTLE)
 
 # SizeOfOptionalHeader (Offset 16, Ukuran 2 byte)
 binary.set(coff_file_header, 16, binary.size(2), endian.LITTLE)
